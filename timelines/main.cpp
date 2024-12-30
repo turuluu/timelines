6b01abfb5ae2a0453d9972604f5c1d08b6a7c061
Joni Turunen <rojun.itu@gmail.com>
2024-12-30 11:12:34 +0200

    add doctest and clean up cmakelists

--- timelines/main.cpp
+++ timelines/main.cpp
@@ -9,29 +9,6 @@
 int
 main(int argc, char** argv)
 {
-#if 0
-    doctest::Context context;
-
-    // !!! THIS IS JUST AN EXAMPLE SHOWING HOW DEFAULTS/OVERRIDES ARE SET !!!
-
-    // defaults
-    context.addFilter("test-case-exclude", "*math*"); // exclude test cases with "math" in their name
-    context.setOption("abort-after", 5);              // stop test execution after 5 failed assertions
-    context.setOption("order-by", "name");            // sort the test cases by their name
-
-    context.applyCommandLine(argc, argv);
-
-    // overrides
-    context.setOption("no-breaks", true);             // don't break in the debugger when assertions fail
-
-    int res = context.run(); // run
-
-    if (context.shouldExit()) // important - query flags (and --exit) rely on the user doing this
-        return res;          // propagate the result of the tests
-
-    int client_stuff_return_code = 0;
-#endif
-#ifndef TESTS_ONLY
     using namespace tls;
     ScopedGraphics sc_g(spec::screen_w, spec::screen_h);
 
@@ -73,9 +50,5 @@ main(int argc, char** argv)
     auto app = Application::own(new sdl::EventHandler(), ui);
     app.loop();
 
-#endif
-
-  // return res + client_stuff_return_code; // the result from doctest is propagated here as well
-
-  // return EXIT_SUCCESS;
+    return 0;
 }
