01c5e520e95e995ae022cde521e579dfdde32e66
Joni Turunen <rojun.itu@gmail.com>
2025-01-10 15:22:09 +0200

    granularity changes

--- src/calendars.hpp
+++ src/calendars.hpp
@@ -13,50 +13,79 @@ idx_t year_to_index(year_t year);
 year_t index_to_year(idx_t index);
 year_t year_limits(year_t year);
 
-struct TimeInterface
+namespace calendar
 {
+enum class Type : unsigned
+{
+    Iso8601 = 0, // Normalized value
+    Gregorian,
+    Julian,
+    Islamic,
+    Hebrew,
+    Coptic,
+    SolarHijri,
+    Bengali,
 };
 
-struct JulianCalendar : TimeInterface
+/**
+ * Iso8601 ~ Gregorian (year 0 = Gregorian year -1 i.e. 1BC)
+ * Gregorian calendar	January 9, 2025
+ * Islamic calendar	9 Rajab, 1446 AH
+ * Hebrew calendar	9 Tevet, AM 5785
+ * Coptic calendar	Tobi 1, 1741 AM
+ * Solar Hijri calendar	20 Dey, 1403 SH
+ * Bengali calendar	Poush 26, 1431 BS
+ * Julian calendar	27 December 2024
+ * Byzantine calendar	9 January 7533
+ */
+template<auto Index>
+struct Base
+{
+    static constexpr auto type = Index;
+};
+
+//
+struct JulianCalendar : Base<Type::Julian>
 {
 };
+}
 
 enum class Granularity : int
 {
-    Metric = -1,
-    Seconds = 0,
+    Metric = -1, // metrics can be scaled in base 10
+    Seconds = 0, // target normalization default for final
     Minutes,
     Hours,
     Days,
     Weeks,
     Months,
-    Years,
+    Years, // wip normalization for experimentation
 };
 
-template<typename IntType>
+template<typename IntType, auto DefaultGranularity>
 struct TimePointT
 {
     using type = IntType;
+
     type t{};
-    type offset{};      // year 1 in gregorian calendar
-    Granularity granularity{}; // seconds
+    type offset{}; // year 0 in ISO8601 calendar
+    Granularity granularity{ DefaultGranularity };
 };
 
+constexpr unsigned long start_of_the_universe_years_since_year_0 = 13'800'000'000UL;
+
 // ISO 8601
-struct Time : TimePointT<size_t>
+struct Time : TimePointT<size_t, Granularity::Years>
 {
     Time()
     {
         t = 1;
-        granularity = Granularity::Years;
-        offset = 13.8 * std::pow(10, 9);
+        offset = start_of_the_universe_years_since_year_0;
     }
 };
 
-
 struct TimeSeries
 {
-
 };
 
 struct Years
