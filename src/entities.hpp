3b154a9026d7d01e9370754397fecb04997604c0
Joni Turunen <rojun.itu@gmail.com>
2025-04-16 21:19:26 +0300

    remove dead code, cleanup

--- src/entities.hpp
+++ src/entities.hpp
@@ -4,19 +4,12 @@
 #include <vector>
 
 #include "time.hpp"
+#include "geometry.hpp"
 
 namespace tls
 {
 static size_t ids = 0;
 
-// TODO : replace with rect
-struct dimensions
-{
-    int x = 0;
-    int y = 0;
-    int w = 0;
-    int h = 0;
-};
 
 struct entity
 {
@@ -26,20 +19,11 @@ struct entity
 
     ~entity();
 
-    enum class property : unsigned
-    {
-        none = 0x0,
-        has_name_and_id = 0x1,
-        has_start = 0x2,
-        has_end = 0x4
-    };
-
     const size_t id;
     std::string name;
     interval interval;
-    property properties = property::none;
 
-    dimensions bounds;
+    rect bounds;
     entity* parent = nullptr;
 };
 
