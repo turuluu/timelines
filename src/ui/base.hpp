a95b0f62b4b2782362ccdebd15036f9e6f5655c4
Joni Turunen <rojun.itu@gmail.com>
2025-04-26 16:18:34 +0300

    Component system using freelist instead of bitset

--- src/ui/base.hpp
+++ src/ui/base.hpp
@@ -22,12 +22,12 @@ struct component_ifc
 {
     component() {}
     virtual ~component() = default;
-    virtual bool click(const context& ctx){};
-    virtual bool cursor(const context& ctx){};
-    virtual bool hit_test(const context& ctx, point p){};
-    virtual void draw(const context& ctx){};
-    virtual void refresh(const context& ctx){};
-    virtual void layout(const context& ctx){};
+    virtual bool click(const context& ctx) {};
+    virtual bool cursor(const context& ctx) {};
+    virtual bool hit_test(const context& ctx, point p) {};
+    virtual void draw(const context& ctx) {};
+    virtual void refresh(const context& ctx) {};
+    virtual void layout(const context& ctx) {};
 };
 
 struct component : component_ifc
@@ -35,58 +35,75 @@ struct component : component_ifc
     sz id;
 };
 
+struct handle
+{
+    sz index = 0;
+    sz version = 0;
+    bool alive = false;
+};
 // SoA structure for caching and mapping
 struct base
 {
-    sz add(component&& new_component)
+    sz add(component&& new_component, context&& ctx)
     {
         sz index = 0;
-        auto it = std::find(component_table.begin(), component_table.end(), false);
-        if (it != component_table.end())
-        {
-            index = std::distance(component_table.begin(), it);
-            component_table.set(index);
-        }
-        else
-        {
-            assert(("component table full", false));
-        }
-
-        if (index == component_table.size())
+        if (freelist.empty())
         {
             components.emplace_back(std::move(new_component));
-            contexts.emplace_back({});
+            contexts.emplace_back(std::move(ctx));
+
+            auto& slot = slots.emplace_back(std::move(handle{ slots.size(), 0, true }));
+            index = slot.index;
         }
         else
         {
+            auto slot = freelist.back();
+            freelist.pop_back();
+
+            slot.version += 1;
+            slot.alive = true;
+            index = slot.index;
+
+            slots[index] = std::move(slot);
             components[index] = std::move(new_component);
-            contexts[index] = {};
+            contexts[index] = std::move(ctx);
         }
-        ++size;
+
+        size = slots.size();
         return index;
     }
     bool remove(sz id)
     {
-        if (component_table[id])
-        {
-            component_table.set(id, false);
-            --size;
-        }
+        if (id >= size || (!freelist.empty() &&
+                           std::find(freelist.begin(), freelist.end(), id) == freelist.end()))
+            return false;
+
+        auto slot = slots[id];
+        slot.alive = false;
+        freelist.emplace_back(std::move(slot));
+
+        return true;
     }
 
     sz size = 0;
-    std::bitset<spec::max_components> component_table;
+    std::vector<handle> freelist;
+    std::vector<handle> slots;
     std::vector<component> components;
     std::vector<context> contexts;
 };
-struct composite_base : component, storage<component>
+struct composite_base
+  : component
+  , storage<component>
 {
-    composite_base(base& base) : base(base) {}
+    composite_base(base& base)
+      : base(base)
+    {
+    }
 
     // TODO : tree of indices matching components and contexts
     // ...
 
     base& base;
 };
-}
-}
+} // namespace ui
+} // namespace tls
